75057053eefbb38c91338dc9309d73166d9b3a21
andie <zchangan@163.com>
2024-05-06 18:43:17 +0800

    item page 10 -> 100

--- common/constants.go
+++ common/constants.go
@@ -28,7 +28,7 @@ var SessionSecret = uuid.New().String()
 var OptionMap map[string]string
 var OptionMapRWMutex sync.RWMutex
 
-var ItemsPerPage = 10
+var ItemsPerPage = 100
 var MaxRecentItems = 100
 
 var PasswordLoginEnabled = true
